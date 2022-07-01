c7e988e362a113943216d508b0034264802a6e2e
njlyon0 <nicholasjlyon@gmail.com>
2022-07-01 11:35:04 -0500

    Used `purrr` instead of reading each file in separately. Specified `tidyverse` namespaces. Changed from `walk` to loop to write to new folders instead of to working directory

--- Papers Search/LTER-transitions-abstracts.R
+++ Papers Search/LTER-transitions-abstracts.R
@@ -24,29 +24,37 @@ folder <- drive_get(as_id(folder_url), shared_drive = "RawSearchResults")
 # identify the csv files in that folder
 csv_files <- drive_ls(folder, type = "csv")
 
+# Create a folder to locally save to
+dir.create("Search Results")
+
 # download them
-walk(csv_files$id, ~ drive_download(as_id(.x), overwrite=TRUE))
+for(csv_id in csv_files$id){
+  # Identify file name
+  csv_name <- dplyr::filter(csv_files, id == csv_id)$name
+  # Download it locally
+  drive_download(file = as_id(csv_id), path = file.path("Search Results", csv_name), overwrite=TRUE)
+}
+# walk(csv_files$id, ~ drive_download(as_id(.x), overwrite=TRUE))
 
 # read in the csv files
-papers1 <- read_csv("Review_FEB232022_to_1K.csv") %>% 
-  select(DOI, ArticleTitle, Abstract)
+## Identify paper names
+csv_names <- dir(file.path("Search Results"))
+csv_names
 
-papers2 <- read_csv("Review_FEB232022_to_2K.csv") %>% 
-  select(DOI, ArticleTitle, Abstract)
-
-papers3 <- read_csv("Review_FEB232022_to_3K.csv") %>% 
-  select(DOI, ArticleTitle, Abstract)
-
-papers4 <- read_csv("Review_FEB232022_to_4K.csv") %>% 
-  select(DOI, ArticleTitle, Abstract)
-
-# combine them altogether and select the columns of interest
-all_papers <- bind_rows(papers1, papers2, papers3, papers4) %>%
-  mutate(DOI = ifelse(is.na(DOI), "no doi", DOI))
+# Read in CSVs
+papers <- file.path("Search Results", csv_names) %>%
+  ## Actual reading step
+  purrr::map(.f = readr::read_csv) %>%
+  ## Then pare down to only needed columns
+  purrr::map_dfr(select, DOI, ArticleTitle, Abstract) %>%
+  ## Then fix missing DOIs
+  dplyr::mutate(DOI = ifelse(test = is.na(DOI),
+                             yes = "no doi",
+                             no = DOI))
 
 # first filter out the papers with the positive keywords
 # then extract the positive and negative keywords from the abstract and make them into 2 new columns
-extracted_papers <- all_papers %>% 
+extracted_papers <- papers %>% 
   mutate(positive_keywords = str_extract_all(Abstract, "[Ee]xperiment|[Oo]bservation"),  #includes words such as "experimental" and "observations"
          negative_keywords = str_extract_all(Abstract, "[Mm]eta-analysis|[Mm]eta analysis|[Mm]odeling|[Mm]odelling|[Rr]eview|[Aa]quatic|[Ii]ncubation|[Gg]reenhouse|[Mm]arine|[Rr]iver|[Cc]oral")) 
 
@@ -74,28 +82,30 @@ extracted_papers <- extracted_papers %>%
 # make a column for every positive keyword, counting how many times each positive keyword appeared in the abstract
 # make one last column counting the total amount of times a positive keyword has appeared in the abstract
 extracted_positive <- extracted_papers %>%  
-  select(-negative_keywords) %>%
-  separate_rows(positive_keywords, sep = ";") %>%
-  mutate(positive_keywords = tolower(positive_keywords)) %>%
-  count(DOI, ArticleTitle, Abstract, positive_keywords) %>%
-  rename(positive_count = n) %>%
-  pivot_wider(names_from = positive_keywords, values_from = positive_count, values_fill = 0) %>%
-  mutate(total_positive_count = experiment+observation) %>%
-  select(-`NA`)
+  dplyr::select(-negative_keywords) %>%
+  tidyr::separate_rows(positive_keywords, sep = ";") %>%
+  dplyr::mutate(positive_keywords = tolower(positive_keywords)) %>%
+  dplyr::count(DOI, ArticleTitle, Abstract, positive_keywords) %>%
+  dplyr::rename(positive_count = n) %>%
+  tidyr::pivot_wider(names_from = positive_keywords,
+                     values_from = positive_count,
+                     values_fill = 0) %>%
+  dplyr::mutate(total_positive_count = experiment+observation) %>%
+  dplyr::select(-`NA`)
 
 # make a column for every negative keyword, counting how many times each negative keyword appeared in the abstract
 # make one last column counting the total amount of times a negative keyword has appeared in the abstract
 # also rename the meta-analysis column to meta_analysis
 extracted_negative <- extracted_papers %>%
-  select(-positive_keywords) %>%
-  separate_rows(negative_keywords, sep = ";") %>%
-  mutate(negative_keywords = tolower(negative_keywords)) %>%
-  count(DOI, ArticleTitle, Abstract, negative_keywords) %>%
-  rename(negative_count = n) %>%
-  pivot_wider(names_from = negative_keywords, values_from = negative_count, values_fill = 0) %>%
-  mutate(total_negative_count = modeling + river + marine + review + modelling + aquatic + `meta-analysis` + coral + incubation + greenhouse) %>%
-  rename(meta_analysis = `meta-analysis`) %>%
-  select(-`NA`) 
+  dplyr::select(-positive_keywords) %>%
+  tidyr::separate_rows(negative_keywords, sep = ";") %>%
+  dplyr::mutate(negative_keywords = tolower(negative_keywords)) %>%
+  dplyr::count(DOI, ArticleTitle, Abstract, negative_keywords) %>%
+  dplyr::rename(negative_count = n) %>%
+  tidyr::pivot_wider(names_from = negative_keywords, values_from = negative_count, values_fill = 0) %>%
+  dplyr::mutate(total_negative_count = modeling + river + marine + review + modelling + aquatic + `meta-analysis` + coral + incubation + greenhouse) %>%
+  dplyr::rename(meta_analysis = `meta-analysis`) %>%
+  dplyr::select(-`NA`) 
 
 
 # combining our results together to get a dataframe that has the total count of positive and negative keywords for each paper's abstract
@@ -124,42 +134,29 @@ folder <- drive_get(as_id(folder_url), shared_drive = "Reading Assignments April
 # identify the spreadsheets in that folder
 sheets <- drive_ls(folder, type = "spreadsheet")
 
+# Create a local folder for them
+dir.create("Reading Assignments", showWarnings = FALSE)
+
 # download them
-walk(sheets$id, ~ drive_download(as_id(.x), overwrite=TRUE))
+for(file_id in sheets$id){
+  # Identify file name
+  file_name <- dplyr::filter(sheets, id == file_id)$name
+  # Download it locally
+  drive_download(file = as_id(file_id), path = file.path("Reading Assignments", file_name), overwrite=TRUE)
+  }
+
+# Identify Excel file names
+xl_names <- dir(file.path("Reading Assignments"))
+xl_names
 
 # read in the google sheets where the PIs have decided whether to include the papers or not
-AnnyApril <- read_excel("AnnyApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-BeatrizApril <- read_excel("BeatrizApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-CarmenApril <- read_excel("CarmenApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-CristyApril <- read_excel("CristyApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-JennApril <- read_excel("JennApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-LauApril <- read_excel("LauApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-LukasApril <- read_excel("LukasApril.xlsx") %>% 
-  select(DOI, ArticleTitle, Abstract, Evaluator, Include) %>% 
-  filter(!is.na(Include))
-
-# combine the sheets altogether 
-all_sheets <- bind_rows(AnnyApril, BeatrizApril, CarmenApril, CristyApril, JennApril, LauApril, LukasApril)  %>%
-  mutate(DOI = ifelse(is.na(DOI), "no doi", DOI))
-
+all_sheets <- file.path("Reading Assignments", xl_names) %>%
+  purrr::map(.f = read_excel) %>%
+  purrr::map_dfr(.f = select, DOI, ArticleTitle, Abstract, Evaluator, Include) %>%
+  dplyr::mutate(DOI = ifelse(test = is.na(DOI),
+                             yes = "no doi",
+                             no = DOI))
+ 
 # left-join our result from earlier with the PIs' decisions
 sanity_check <- left_join(result, all_sheets, by = c("DOI", "ArticleTitle", "Abstract")) %>%
   filter(!is.na(Include)) 
