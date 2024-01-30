89b13b1287d202a36a91824895fb4d2515758aac
yyachung <yyachung@gmail.com>
2024-01-30 17:33:34 -0500

    even more files

---
+++ Read_cleandata.R
@@ -0,0 +1,11 @@
+source("Data_cleaning/cdr_e001_e002_data_cleaning.R")
+source("Data_cleaning/cdr_e141_data_cleaning.R")
+source("Data_cleaning/jrn_pvar_data_cleaning.R") # extra column is amb rainfall
+# source("Data_cleaning/knz_rmp_data_cleaning.R")
+source("Data_cleaning/nwt_fert_data_cleaning.R")
+source("Data_cleaning/nwt_itex_data_cleaning.R")
+source("Data_cleaning/nwt_recovery_data_cleaning.R")
+source("Data_cleaning/sev_mrme_data_cleaning.R")
+source("Data_cleaning/sev_wennedex_data_cleaning.R")
+source("Data_cleaning/sev_nfert_data_cleaning.R")
+source("Data_cleaning/kbs_early_succ_microplot_msce_cleaning.R") # extra column is disturbance treatment
