1ebf09f197c73769bc1769b3355c35224d1bf4b6
Megan Wilcots <megan.wilcots@gmail.com>
2024-01-31 10:21:20 -0800

    Comparing Nick's plotting method vs. visreg

--- Project_3_climate_sensitivity/individual sites CSF /csf_yarra.R
+++ Project_3_climate_sensitivity/individual sites CSF /csf_yarra.R
@@ -28,10 +28,23 @@ visreg(lm.Ca, xvar = "spei", type = "conditional", by = "trt_type", data = yarra
   geom_point(aes(x = spei, y = anpp, color = trt_type), alpha = 0.2, data = yarra) +
   theme_bw() +
   labs(x="SPEI",
-       y="ANPP")
+       y="ANPP") +
+  scale_y_continuous(limits = c(0,1000))
+fits <- data.frame("uniqueID" = names(fitted(object = m.Ca)),
+                   "anpp_model_fits" = fitted(object = m.Ca))
 
-ggplot(yarra, aes(x = spei, y = anpp, color = trt_type)) +
-  geom_point() +
-  #  facet_wrap(~p) +
-  geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3), span = 1, se = F) +
-  labs(y = "ANPP")
+# Drop rownames (purely for aesthetic reasons)
+rownames(fits) <- NULL
+
+# Bind onto "real" data
+yarra_fits <- dplyr::left_join(x = yarra, y = fits, by = "uniqueID")
+
+# Make desired plot
+ggplot(data = yarra_fits, aes(x = spei, y = anpp_model_fits, color = trt_type)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3), se = F) +
+  #facet_wrap( ~ n) +
+  theme_bw() +
+  labs(x="SPEI",
+       y="ANPP") +
+  scale_y_continuous(limits = c(0,1000))
