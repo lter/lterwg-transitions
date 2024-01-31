93cba424ec9fce8f841a19c06abb3034bdab2f47
Megan Wilcots <megan.wilcots@gmail.com>
2024-01-31 14:38:15 -0800

    make dfs

--- Project_3_climate_sensitivity/spei_coverage.R
+++ Project_3_climate_sensitivity/spei_coverage.R
@@ -38,3 +38,31 @@ unique(corre_data$year) ## CoRRE years -- 1986 - 2022
 names(all_SPEI_raw)
 all_SPEI_raw <- all_SPEI_raw %>%
   dplyr::select(site_code, year, spei, lon, lat, month)
+
+dont_include <- c("watering", "e001", "e002", "PHACE", "Yu", "IRG", "TMECE", "Nfert", "RaMPs")
+'%!in%' <- function(x,y)!('%in%'(x,y)) ## from file cleaning
+
+corre_sites <- corre_data %>%
+  filter(trt_type == "control" | trt_type == "N" | trt_type == "P" | trt_type == "N*P") %>% ## only keep fert treatments
+  filter(project_name %!in% dont_include) %>% ## see above
+  filter(site_code != "temple.us") 
+corre_site_names <- unique(corre_sites$site_code)
+
+## sites that harvest in August
+august_gs <- c("KNZ", "KBS", "CDR", "cbgb.us", "KUFS", "NWT")
+
+data_corre <- left_join(corre_sites, all_SPEI_raw)  %>%
+  mutate(month_keep = ifelse(site_code %in% august_gs, 8, 
+                             ifelse(site_code == 'yarra.us', 4, 
+                                    ifelse(site_code == "SERC", 10, 5)))) %>%
+  filter(month == month_keep) %>%
+  dplyr::select(-month_keep)
+
+spei_100 <- all_SPEI_raw %>%
+  filter(site_code %in% corre_site_names) %>%
+  mutate(month_keep = ifelse(site_code %in% august_gs, 8, 
+                             ifelse(site_code == 'yarra.us', 4, 
+                                    ifelse(site_code == "SERC", 10, 5)))) %>%
+  filter(month == month_keep) %>%
+  dplyr::select(-month_keep)
+  
