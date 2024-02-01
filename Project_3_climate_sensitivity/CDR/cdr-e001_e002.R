d475817a8c0f59ad98b5cfe80d5f269a4740f951
Megan Wilcots <megan.wilcots@gmail.com>
2024-02-01 11:22:56 -0800

    cleaning CDR data

--- Project_3_climate_sensitivity/CDR/cdr-e001_e002.R
+++ Project_3_climate_sensitivity/CDR/cdr-e001_e002.R
@@ -1,1 +1,12 @@
 ### 
+
+cdr <- read.csv(here::here("Project_3_climate_sensitivity", "CDR", "cdr_clean.csv"))
+
+cdr1 <- cdr %>%
+  filter(ncess == 0) %>% ##only want N addition
+  rename(n = nadd) %>%
+  mutate(site_code = rep("CDR")) %>%
+  group_by(year, site_code, plot, uniqueID, n) %>%
+  summarize(anpp = sum(abundance)) %>%
+  ungroup()
+cdr1$p <- rep(20, times = nrow(cdr1))
